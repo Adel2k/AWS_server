8edeaed3a0c4af8ed04bafcab1f7cdcee84308f8
Adeline <adelineeminian13@gmail.com>
2024-11-16 17:27:59 +0400

    edited

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@
 FILE=/home/ubuntu/importent_file/*
 formatted_date=$(date +"%Y-%m-%d %H:%M:%S")
 zip -r "$FILE-$formatted_date.zip" $FILE
-mkdir back_ups
+mkdir -p back_ups
 mv /home/ubuntu/importent_file/*.zip back_ups
